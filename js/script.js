cb32a4592f3ac602a33a79f9022df1bb3a51d211
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-25 15:52:24 +0100

    reactive_contact_chat

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 console.log("Hello World!", Vue);
 
-const { createApp } = Vue
+const {user, contacts} = data;
+
+const { createApp } = Vue;
 
 const app = createApp({
   data: () => ({
     name: "Boolzap",
-    data,
-  })
+    activeContactID: 1,
+    contacts,
+    user,
+  }),
+  computed: {
+    currentActiveID(){
+      const currentActiveContact = this.contacts.find(contact => {
+        return contact.id === this.activeContactID;
+      });
+    }
+  },
+  methods: {
+    setCurrentActiveID(id) {
+      this.activeContactID = id;
+      console.log(this.activeContactID);
+    }
+  }
 })
 
 app.mount('#root')
