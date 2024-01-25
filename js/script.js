2a12cee798b8fd7b0b9a025ab287bb861bf9a3c4
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-25 18:08:38 +0100

    send_message

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ console.log("Hello World!", Vue);
 
 const {user, contacts} = data;
 
+const chatWindow = document.getElementById(".chat-window");
+
 const { createApp } = Vue;
 
 const app = createApp({
@@ -10,6 +12,7 @@ const app = createApp({
     activeContactID: 1,
     contacts,
     user,
+    newMessage: ""
   }),
   computed: {
     currentContact(){
@@ -17,11 +20,30 @@ const app = createApp({
         if (contact.id === this.activeContactID) return contact;
       }); 
       return currentContact;
-    }
+    },
   },
   methods: {
     setCurrentActiveID(id) {
       this.activeContactID = id;
+    },
+    sendMessage() {
+      const date = new Date();
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const year = String(date.getFullYear());
+      const hour = String(date.getHours()).padStart(2, "0");
+      const minutes = String(date.getMinutes()).padStart(2, "0");
+      const seconds = String(date.getSeconds()).padStart(2, "0");
+
+      const currentDate = `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`
+
+      this.currentContact.messages.push({
+        id: this.currentContact["messages"].length+1,
+        date: currentDate,
+        text: this.newMessage,
+        status: 'sent'
+      })
+      this.newMessage = "";
     }
   }
 })
