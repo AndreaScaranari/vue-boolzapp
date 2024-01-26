86d06731fb31d9b4439fa5e4bc85a4c1a5229316
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-26 11:36:59 +0100

    validation_window_chat

--- js/script.js
+++ js/script.js
@@ -2,26 +2,12 @@ console.log("Hello World!", Vue);
 
 const {user, contacts} = data;
 
-const chatWindow = document.getElementById(".chat-window");
-
-const getSendingTiming = () => {
-    const date = new Date();
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = String(date.getFullYear());
-    const hour = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-
-    return currentDate = `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`
-  }
-
 const { createApp } = Vue;
 
 const app = createApp({
   data: () => ({
     name: "Boolzap",
-    activeContactID: 1,
+    activeContactID: null,
     contacts,
     user,
     newMessage: "",
@@ -30,10 +16,7 @@ const app = createApp({
   }),
   computed: {
     currentContact(){
-      const currentContact = this.contacts.find(contact => {
-        if (contact.id === this.activeContactID) return contact;
-      }); 
-      return currentContact;
+      return this.contacts.find(contact => contact.id === this.activeContactID);
     },
     filteredContacts(){
       const searchTerm = this.searchText.toLowerCase();
@@ -47,26 +30,38 @@ const app = createApp({
     setCurrentActiveID(id) {
       this.activeContactID = id;
     },
-    sendMessage() {
-      if (this.newMessage){
-      this.currentContact.messages.push({
-        id: this.currentContact["messages"].length+1,
-        date: getSendingTiming(),
-        text: this.newMessage,
-        status: 'sent'
-      })
-      this.newMessage = "";}
+    getSendingTiming() {
+      const date = new Date();
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const year = String(date.getFullYear());
+      const hour = String(date.getHours()).padStart(2, "0");
+      const minutes = String(date.getMinutes()).padStart(2, "0");
+      const seconds = String(date.getSeconds()).padStart(2, "0");
+  
+      return currentDate = `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`
     },
-    getOKAnswer(){
+    prepareMessage(text, status) {
+      this.currentContact.messages.push({
+        id: new Date().getTime(),
+        date: this.getSendingTiming(),
+        text,
+        status,
+      })
+    },
+    sendMessage() {
+      if (!this.newMessage) return
+      this.prepareMessage(this.newMessage, "sent");
+
+      this.newMessage = "";
+ 
       this.autoAnswerTiming = setTimeout(() => {
-        this.currentContact.messages.push({
-        id: this.currentContact["messages"].length+1,
-        date: getSendingTiming(),
-        text: "Ok!",
-        status: 'received'
-        })
+        this.prepareMessage("Ok!", "received");
       }, 1000);
     }
+  },
+  created() {
+    this.activeContactID = this.contacts[0].id;
   }
 })
 
